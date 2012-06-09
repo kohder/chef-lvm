7f3a8bc3e86d4ea15863b953902ce784ae919313
Motiejus Jakštys <desired.mta@gmail.com>
2012-06-09 19:17:34 +0200

    Update Metadata (bump version and README)

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer       "Rob Lewis <rob@kohder.com>"
 maintainer_email "rob@kohder.com"
 license          "Apache 2.0"
 description      "Installs/Configures lvm"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.8.2"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.9.0"
 %w{ debian ubuntu }.each do |os|
   supports os
 end
