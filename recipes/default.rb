8b5a50edaa33fb95c02ff4d7eb283244e2e00853
Motiejus Jakštys <desired.mta@gmail.com>
2012-06-09 19:09:22 +0200

    Add a better default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,3 +20,31 @@
 package 'lvm2' do
   action :upgrade
 end
+
+lvm_pv "create physical volumes" do
+    devices node['lvm']['on']
+end
+
+lvm_vg "create volume groups" do
+    devices node['lvm']['on']
+end
+
+node['lvm']['logical_volumes'].each do |name,data|
+    lvm_lv "create logical volumes" do
+
+        logical_volume_name name
+
+        if data.key? :volume_group_name then
+            volume_group_name data[:volume_group_name]
+        end
+
+        stripes data[:stripes] if data.key? :stripes
+        stripes_size data[:stripes_size] if data.key? :stripes_size
+
+        if data.key? :logical_extents then
+            logical_extents data[:logical_extents]
+        end
+
+        size data['size'] if data.key? :size
+    end
+end
